5b41eb1b5b751c2a9cfffb41454f8bfa069bc5a8
Seth Bradshaw <seth.bradshaw@carvana.com>
2022-11-27 15:31:09 -0700

    adding transfer device, pause, and resume API calls

--- spotify-ui/src/constants/endpoints.ts
+++ spotify-ui/src/constants/endpoints.ts
@@ -1,10 +1,17 @@
 // TODO update this with env file to determine base URL
 const mockSpotifyBaseURL = 'http://localhost:8080';
 
+// * AUTH ENDPOINTS
 const authURL = `${mockSpotifyBaseURL}/auth`;
-const playbackURL = `${mockSpotifyBaseURL}/playback`
-export const CURRENT_PLAYBACK_STATE_URL =`${playbackURL}/current-state`
-export const GET_PLAYBACK_DEVICES = `${playbackURL}/device-list`
+
 export const LOGIN = `${authURL}/userLogin`;
 // * requires param refresh_token
 export const REFRESH_TOKEN = `${authURL}/refresh-token`;
+
+// * PLAYBACK ENDPOINTS
+const playbackURL = `${mockSpotifyBaseURL}/playback`
+export const CURRENT_PLAYBACK_STATE_URL =`${playbackURL}/current-state`
+export const GET_PLAYBACK_DEVICES = `${playbackURL}/device-list`
+export const TRANSFER_DEVICE = `${playbackURL}/transfer-device`
+export const PLAY_TRACK = `${playbackURL}/play-track`
+export const PAUSE_TRACK = `${playbackURL}/pause-track`
