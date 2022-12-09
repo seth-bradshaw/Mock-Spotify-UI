e6370d89a29435c16f1588d5f1b87589182617e0
Seth Bradshaw <seth.bradshaw@carvana.com>
2022-12-09 14:33:47 -0700

    setup tailwind config

---
+++ spotify-ui/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'spotify-green-400': '#1ED760',
+        'spotify-green-500': '#1DD05D',
+        'spotify-gray-200': '#A7A7A7',
+        'spotify-gray-300': '#B3B3B3',
+        'spotify-gray-600': '#5D5D5D',
+        'spotify-gray-800': '#181818',
+        'spotify-gray-900': '#121212'
+      }
+    },
+  },
+  plugins: []
+}
