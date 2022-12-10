e92e8bd1297ff24fb14e81f35c0b08c35a854ccb
William Fletcher <wfletch7@gmail.com>
2022-12-09 19:35:30 -0500

    created context hook

--- spotify-ui/src/components/pages/Home.tsx
+++ spotify-ui/src/components/pages/Home.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
-
+import React, {useEffect} from 'react'
+import SpotifyPlayback from '../playback/SpotifyPlayback'
+import PlaybackContextProvider from '../playback/PlaybackContext'
 export default function Home() {
+  useEffect(() => {
+    console.log('inside seths butt')
+  }, [])
+  console.log('outside seths butt')
   return (
     <div>
-
+      <PlaybackContextProvider/>
     </div>
   )
 }
