96a929127dc42871e2d62a54b30827d95fc3d278
seth-bradshaw <seth.bradshaw27@gmail.com>
2022-12-19 13:36:53 -0700

    final touches on base slider

--- spotify-ui/src/components/common/BaseSlider.tsx
+++ spotify-ui/src/components/common/BaseSlider.tsx
@@ -1,51 +1,56 @@
-import React, { FormEvent, ReactElement } from "react";
+import React, { FormEvent, ReactElement, useEffect, useRef, useState } from "react";
 
 import "../../slider.css";
 
-export const updateBackground = (e: FormEvent, bgColor: string = "#1ED760") => {
-  const target = e.target as HTMLInputElement;
-  var value =
-    ((Number(target.value) - Number(target.min)) /
-      (Number(target.max) - Number(target.min))) *
-    100;
-  target.style.background = `linear-gradient(to right, ${bgColor} 0%, ${bgColor} ${value}%, #A7A7A7 ${value}%, #A7A7A7 100%)`;
-};
-
+// * types
 interface Props {
   defaultValue: number;
   handleChange: (e: FormEvent<HTMLInputElement>) => void;
   id: string;
+  value: number;
+  max?: string;
+  className?: string;
+}
+
+enum BackgroundColor {
+  white = '#ffffff',
+  green = "#1ED760"
 }
 
 export default function BaseSlider({
-  defaultValue = 0,
   handleChange,
   id,
+  className = '',
+  max = '100',
+  value
 }: Props): ReactElement {
-  const handleClick = (e: FormEvent<HTMLInputElement>) => {
-    handleChange(e);
-  };
+  const [background, setBackground] = useState<BackgroundColor>(BackgroundColor.white);
+  // * reference to input, avoids needing to select by id
+  const inputRef = useRef<HTMLInputElement | null>(null); 
+
+  useEffect(() => {
+  // * calcs percentage of either BackgroundColor options to display
+    const percent = (value / Number(max)) * 100;  
+
+    if (inputRef.current) {
+      // * update input background to display BackgroundColor option x percent
+      inputRef.current.style.background = `linear-gradient(to right, ${background} 0%, ${background} ${percent}%, #A7A7A7 ${percent}%, #A7A7A7 100%)`;
+    }
+  }, [value, background])
 
   return (
     <input
+      ref={inputRef}
       id={id}
-      className="slider"
+      className={`slider ${className}`}
       type="range"
       min="0"
-      max="100"
-      defaultValue={defaultValue}
-      onInput={(e: FormEvent<HTMLInputElement>) => {
-        updateBackground(e);
-        handleClick(e);
-      }}
-      onClick={(e: FormEvent<HTMLInputElement>) => {
-        updateBackground(e);
-        handleClick(e);
-      }}
-      onMouseOver={updateBackground}
-      onMouseLeave={(e: FormEvent<HTMLInputElement>) =>
-        updateBackground(e, "#ffffff")
-      }
+      max={max}
+      value={value}
+      onInput={(e: FormEvent<HTMLInputElement>) => handleChange(e)}
+      onClick={(e: FormEvent<HTMLInputElement>) => handleChange(e)}
+      onMouseOver={() => setBackground(BackgroundColor.green)}
+      onMouseLeave={() => setBackground(BackgroundColor.white)}
     />
   );
 }
