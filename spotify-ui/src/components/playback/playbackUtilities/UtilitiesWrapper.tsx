d5bebde1f761c0f9e0822514713ddb09e1a11533
seth-bradshaw <seth.bradshaw27@gmail.com>
2022-12-20 13:16:51 -0700

    removed console log

--- spotify-ui/src/components/playback/playbackUtilities/UtilitiesWrapper.tsx
+++ spotify-ui/src/components/playback/playbackUtilities/UtilitiesWrapper.tsx
@@ -7,7 +7,6 @@ interface Props {
 }
 
 export default function UtilitiesWrapper({}: Props): ReactElement {
-  console.log('rendering utilities')
   return (
     <div className="basis-1/3 flex justify-end items-center text-lg gap-4">
       <Queue />
