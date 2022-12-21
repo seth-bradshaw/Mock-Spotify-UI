ef8ce7d0617e63d7ee38dca209bdc07200137248
seth-bradshaw <seth.bradshaw27@gmail.com>
2022-12-20 22:58:49 -0700

    update existing component css to work with page layout

--- spotify-ui/src/components/pages/Home.tsx
+++ spotify-ui/src/components/pages/Home.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import MainContent from '../mainView'
+import NavMenu from '../navMenu'
 import PlaybackSection from '../playback'
+import './home.css'
 
 export default function Home() {
   
   return (
-    <div>
+    <div id="home-page" className="h-full home-layout">
+      <NavMenu />
+      <MainContent />
       <PlaybackSection />
     </div>
   )
