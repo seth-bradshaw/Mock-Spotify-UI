ccbe64fe1501db8c835ba62d9f9b9a3d944ab70a
seth-bradshaw <seth.bradshaw27@gmail.com>
2022-12-23 11:54:38 -0700

    demonstrated simple level implementation pattern for views

--- spotify-ui/src/components/mainView/View.tsx
+++ spotify-ui/src/components/mainView/View.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
-import { Outlet, useParams } from 'react-router'
+import { Outlet, useParams, useNavigate } from 'react-router'
 
 type Props = {}
 
-export default function View({}: Props) {
-
+export default function View(props: Props) {
+  const params = useParams();
+  const navigate = useNavigate();
+  console.log('params', {route: params['*']?.slice(0, params['*'].indexOf('/')), props})
   return (
     <div className='row-span-1 overflow-y-auto relative bg-spotify-gray-800'>
+      {/* TODO view sticky header goes here */}
+      <div className='h-24 w-full bg-white fixed'>
+        <button type="button" onClick={() => navigate(-1)}>go back</button>
+      </div>
       <Outlet />
     </div>
   )
