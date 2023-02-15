8894f84965509e2db76380dc64562e47cfdbc3b7
seth-bradshaw <seth.bradshaw27@gmail.com>
2023-02-15 15:41:12 -0700

    added artist slice, and fetch followed artists service call and thunk

--- spotify-ui/src/App.tsx
+++ spotify-ui/src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Route, Routes } from 'react-router';
 import View from './components/mainView';
 import Artist from './components/mainView/views/Artist';
--- spotify-ui/src/constants/types/artist.ts
+++ spotify-ui/src/constants/types/artist.ts
@@ -1,9 +1,16 @@
 import { Artists } from "../../components/playback/context/types";
 import { ExternalUrls } from "./common";
 
+type Followers = {
+  href: string;
+  total: number
+}
 export interface Artist extends Artists {
   type: string;
   id: string;
   href: string;
   external_urls: ExternalUrls;
+  followers?: Followers;
+  genres?: Array<String>;
+  images?: Array<{url: string; height: number; width: number;}>
 }
