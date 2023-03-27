309dbbcd9c03a65e0ca3681a4ee67009d2ee9d15
seth-bradshaw <seth.bradshaw27@gmail.com>
2023-03-27 14:27:11 -0700

    updated home view, added mock sticky header

--- spotify-ui/src/components/mainView/View.tsx
+++ spotify-ui/src/components/mainView/View.tsx
@@ -1,18 +1,12 @@
-import React from 'react'
-import { Outlet, useParams, useNavigate } from 'react-router'
+import { Outlet } from 'react-router'
+import StickyHeader from './views/ViewLayout/StickyHeader'
 
 type Props = {}
 
 export default function View(props: Props) {
-  const params = useParams();
-  const navigate = useNavigate();
-  console.log('params', {route: params['*']?.slice(0, params['*'].indexOf('/')), props})
   return (
-    <div className='row-span-1 overflow-y-auto relative bg-spotify-gray-800'>
-      {/* TODO view sticky header goes here */}
-      <div className='h-24 w-full bg-white fixed'>
-        <button type="button" onClick={() => navigate(-1)}>go back</button>
-      </div>
+    <div className='row-span-1 overflow-y-auto relative bg-spotify-gray-900'>
+      <StickyHeader />
       <Outlet />
     </div>
   )
