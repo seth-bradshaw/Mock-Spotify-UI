2deb65062798763b1cbcb0ebe9ddf4430d405f57
seth-bradshaw <seth.bradshaw27@gmail.com>
2023-07-28 21:27:02 -0700

    updated general components to support the Album view

--- spotify-ui/src/components/mainView/views/ViewLayout/HeaderSection.tsx
+++ spotify-ui/src/components/mainView/views/ViewLayout/HeaderSection.tsx
@@ -16,18 +16,18 @@ export default function HeaderSection({
   imgSrc,
 }: Props) {
   return (
-    <div className="h-96" style={{ backgroundColor }}>
+    <div className="mt-10 h-80 flex flex-col items-end" style={{ backgroundColor }}>
       <div className="w-full bg-transparent text-white flex items-center h-full p-6 gap-10">
         <img
           id="artist-img"
-          className="object-scale-down bg-center bg-no-repeat h-64 w-64 rounded-full"
+          className="object-cover bg-center bg-no-repeat h-64 w-64"
           src={imgSrc}
           crossOrigin="anonymous"
           alt="Image of artist, album cover, or song."
         ></img>
         <div className="flex flex-col justify-left">
           {label && <p className="text-md">{label}</p>}
-          <h1 className="text-[72px] font-bold mb-10">{header}</h1>
+          <h1 className="text-[72px] font-bold mb-6">{header}</h1>
           <h3 className="text-lg">{subheader}</h3>
         </div>
       </div>
