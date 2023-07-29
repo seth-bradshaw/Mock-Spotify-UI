ab1c4cf3f26edc29efc79ba24c4fe168a996c9f9
seth-bradshaw <seth.bradshaw27@gmail.com>
2023-07-28 21:28:00 -0700

    supposed to be included with last commit

--- spotify-ui/src/components/mainView/views/ViewLayout/Section.tsx
+++ spotify-ui/src/components/mainView/views/ViewLayout/Section.tsx
@@ -9,14 +9,19 @@ type Props = {
 export default function Section({ title = '', link = '', linkHref = '', children }: PropsWithChildren<Props>) {
   return (
     <div className="mb-10">
-      <div className="w-full flex justify-between h-12 text-white">
-        <h4 className="text-2xl hover:underline">{title}</h4>
-        {
-          link.length > 0 && (
-            <a className="text-md no-underline hover:underline text-spotify-gray-300" href={linkHref}>{link}</a>
-          )
-        }
-      </div>
+      {
+        title || link && (
+
+          <div className="w-full flex justify-between h-12 text-white">
+            <h4 className="text-2xl hover:underline">{title}</h4>
+            {
+              link.length > 0 && (
+                <a className="text-md no-underline hover:underline text-spotify-gray-300" href={linkHref}>{link}</a>
+              )
+            }
+          </div>
+        )
+      }
       {children}
     </div>
   )
